7d82170d7d793b191048ea0fd9046a01d8d11469
Kelly <kelly@MacBook-Pro.local>
2025-02-14 15:18:06 +0700

    feat 2 commit 2

--- script.ts
+++ script.ts
@@ -14,3 +14,9 @@
 //feat 2 feat 1 dev
 // test change dev
 // test change dev
+// test change dev
+// test change dev
+// test change dev
+//feat 2 feat 1 dev 2
+//feat 2 feat 1 dev 2
+// test change dev
